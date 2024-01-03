1e835e7a253e959309b03fe391cdad90481d0d7b
joelsteven <joelofelectronics@gmail.com>
2024-01-04 00:03:02 +0300

    Add async/await function for uploading user with photo

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+
+    return { photo, user };
+  } catch (err) {
+    photo = null;
+    user = null;
+    return { photo, user };
+  }
+}
