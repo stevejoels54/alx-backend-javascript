07b8b2e10c6cdbb6b4e78e9b1b7eb327269ebd23
joelsteven <joelofelectronics@gmail.com>
2024-01-04 20:20:24 +0300

    Add input validation for HolbertonCourse constructor

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,17 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
