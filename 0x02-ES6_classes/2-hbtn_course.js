0eb615c75a1e8cad249682276f5e97d743a33297
joelsteven <joelofelectronics@gmail.com>
2024-01-04 20:17:38 +0300

    Add HolbertonCourse class with getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (!newStudents.every((student) => typeof student === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
