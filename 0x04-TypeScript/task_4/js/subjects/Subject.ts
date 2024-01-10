0e8ac65a9e1f6ad272eea858321cb09aef559ac6
joelsteven <joelofelectronics@gmail.com>
2024-01-11 01:02:49 +0300

    Add task_5 TypeScript configuration and new subject classes

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,14 @@
+namespace Subjects {
+    export class Subject {
+      private _teacher: Teacher | undefined;
+  
+      setTeacher(teacher: Teacher): void {
+        this._teacher = teacher;
+      }
+  
+      get teacher(): Teacher | undefined {
+        return this._teacher;
+      }
+    }
+  }
+  
