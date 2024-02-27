f62f83ecf86c0fee79c8667b06769839d546324c
joelsteven <joelofelectronics@gmail.com>
2024-02-27 23:10:06 +0300

    fix string formats

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
