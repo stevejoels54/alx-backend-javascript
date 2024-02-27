65b002f43e5486a1115ee118036ea35b999c1a41
joelsteven <joelofelectronics@gmail.com>
2024-02-28 02:59:34 +0300

    Add Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const host = '127.0.0.1';
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.use((req, res) => {
+  res.status(404).send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://${host}:${port}`);
+});
+
+module.exports = app;
