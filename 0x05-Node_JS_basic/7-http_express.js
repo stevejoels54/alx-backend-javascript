ad7fb2a719054e6e0b4e92de13aa8031a552362b
joelsteven <joelofelectronics@gmail.com>
2024-02-28 03:24:31 +0300

    Add Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const host = '127.0.0.1';
+const port = 1245;
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(DB_FILE)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://${host}:${port}`);
+});
+
+module.exports = app;
