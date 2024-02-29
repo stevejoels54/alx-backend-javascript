e36c913983ec1fb7d67228baa1e523e7c84c51d3
joelsteven <joelofelectronics@gmail.com>
2024-03-01 01:07:28 +0300

    Add API code and unit tests

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('sendPaymentRequestToApi', function() {
+    it('Should return payment methods for cart with a valid id', function(done) {
+        const url = 'http://localhost:7865';
+        request(url + '/cart/12', function(error, response, body) {
+            if (error) {
+                done(error);
+            }
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 12');
+            done();
+        });
+    });
+
+    // test for invalid id
+    it('Should return 404 for invalid id', function(done) {
+        const url = 'http://localhost:7865';
+        request(url + '/cart/hello', function(error, response, body) {
+            if (error) {
+                done(error);
+            }
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+});
